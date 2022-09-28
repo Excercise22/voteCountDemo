05543eb3fc60ab659510663a9284a38e22523abe
Abha <abhamshah@hotmail.com>
2022-09-28 17:15:29 -0500

    added logic for creating initial buckets for candidates, finding potential candidate for elimination and determining win if any.

--- src/main/java/com/oracle/interview/evote/service/BallotService.java
+++ src/main/java/com/oracle/interview/evote/service/BallotService.java
@@ -4,9 +4,11 @@ import com.oracle.interview.evote.domain.Ballot;
 import com.oracle.interview.evote.domain.Voter;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BallotService implements BallotInterface {
     private static Map<Character, String> candidates;
+    private static final int CANDIDATE_ELIMINATION_TAG = -1;
 
     public List<Ballot> createBallots(List<Voter> voterList) throws  Exception {
         candidates = CandidateRepositoryService.getCandidatesWithPrefix(null);
@@ -50,6 +52,27 @@ public class BallotService implements BallotInterface {
     }
 
     public Map<String, List<Ballot>> createCandidatesBucket(List<Ballot> ballots){
-        return null;
+        Map<String, List<Ballot>> candidateBucket = new HashMap<>();
+        for (int index = 0; index < ballots.size(); index++) {
+            Optional<String> candidate = ballots.get(index).getVoteMap().entrySet().stream().filter(e -> e.getValue() == 1).map(e -> e.getKey()).findFirst();
+            List<Ballot> ballotArrayList = new ArrayList<>();
+            if (candidate.isPresent()) {
+                List<Ballot> candidatePrefBallots = candidateBucket.get(candidate.get());
+                if (candidatePrefBallots == null) {
+                    candidateBucket.put(candidate.get(), Collections.singletonList(ballots.get(index)));
+
+                } else {
+                    ballotArrayList.addAll(candidatePrefBallots);
+                    ballotArrayList.add(ballots.get(index));
+                    candidateBucket.put(candidate.get(), ballotArrayList);
+                }
+            }
+        }
+        System.out.println("***** Round 1 *****");
+        candidateBucket.entrySet().forEach(entry -> {
+            System.out.println(String.format("Candidate %s having votes %d", entry.getKey(), entry.getValue().size()));
+        });
+        return candidateBucket;
     }
+
 }
--- src/main/java/com/oracle/interview/evote/service/TallyService.java
+++ src/main/java/com/oracle/interview/evote/service/TallyService.java
@@ -3,17 +3,49 @@ package com.oracle.interview.evote.service;
 import com.oracle.interview.evote.domain.Ballot;
 import com.oracle.interview.evote.domain.Voter;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class TallyService implements TallyServiceInterface {
+    private static final String NO_VALID_BALLOTS_FOUND = "No Valid Ballots Found. Please check entered choices";
     BallotInterface ballotInterface = new BallotService();
+
+    private Optional<String> hasClearWin(Map<String, List<Ballot>> candidatesBucket) {
+        Optional<String> isWinner = Optional.empty();
+        int nonExhaustedBallot = (int) candidatesBucket.entrySet().stream().map(c -> c.getValue()).flatMap(List::stream).count();
+        int quota = (nonExhaustedBallot / 2) + 1;
+        System.out.println("Quota is " + quota);
+        Optional<Map.Entry<String, List<Ballot>>> winningCandidate = candidatesBucket.entrySet().stream().filter(s -> s.getValue().size() >= quota).findFirst();
+        if (winningCandidate.isPresent()) {
+            System.out.println("***** Winner *****");
+            System.out.println("Candidate " + winningCandidate.get().getKey() + " selected as Winner");
+            isWinner = Optional.of(winningCandidate.get().getKey());
+        }
+        return isWinner;
+    }
+
+    private String findPotentialElimination(Map<String, List<Ballot>> candidatesBucket) {
+        Map.Entry<String, List<Ballot>> candidateWithLatestPrefCount =
+                candidatesBucket.entrySet().stream().sorted(Comparator.comparingInt(s -> s.getValue().size())).toList().get(0);
+        return candidateWithLatestPrefCount.getKey();
+    }
+
     public String beginTally(List<Voter> voterList) throws Exception {
         int round = 1;
         List<Ballot> ballots = ballotInterface.createBallots(voterList);
+
         if(ballots.size() != 0) {
             Map<String, List<Ballot>> candidatesBucket = ballotInterface.createCandidatesBucket(ballots);
+            round++;
+            Optional<String> winner = hasClearWin(candidatesBucket);
+           /* while (winner.isEmpty()) {
+                String candidatesToEliminate = findPotentialElimination(candidatesBucket);
+            }*/
+            return winner.get();
+        } else {
+            return NO_VALID_BALLOTS_FOUND;
         }
-        return null;
     }
 }
